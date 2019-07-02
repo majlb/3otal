8bd7a0c2779c2f6f8fdd83c4c4bfe0b4056e15a8
Mohamad Jarrah <mja@sword-lebanon.com>
2019-07-02 07:07:34 +0300

    first commit

---
+++ AR.php
@@ -0,0 +1,27 @@
+<?php
+$lang_arr = array();
+
+$lang_arr['HomePage'] = 'الصفحة الأساسية';
+$lang_arr['workDay'] = 'يوم عمل عادي';
+$lang_arr['vacation'] = 'عطلة';
+$lang_arr['eventNoVacation'] = 'مناسبة من دون عطلة';
+$lang_arr['weekEnd'] = 'عطلة نهاية الإسبوع';
+$lang_arr['allArabCountries'] ='جميع الدول العربية';
+$lang_arr['eastArabCountries'] ='المشرق العربي';
+$lang_arr['westArabCountries'] ='المغرب العربي';
+$lang_arr['otalArabVacations'] ='العطل في الدول العربية';
+$lang_arr['3otal'] ='عطل';
+$lang_arr['allAboutArabVacations'] ='كل ما يتعلق بالعطل في الدول العربية';
+$lang_arr['checkArabVacations'] ='يمكنك التحقق من جميع الإجازات والعطل العربية';
+$lang_arr['aboutUs'] ='عن الصفحة';
+$lang_arr['byMap'] ='بالخريطة';
+$lang_arr['byCalendar']= 'بالتقويم';
+$lang_arr['happenThisDay']='حدث في هذا اليوم';
+$lang_arr['contactUs']='تواصل معنا';
+$lang_arr['allVac']='كل العطل';
+$lang_arr['date']='التاريخ';
+$lang_arr['hoverCountry']='يمكنك المرور على خريطة البلد للحصول على المعلومات';
+$lang_arr['noIncidents'] ='لا أحداث';
+$lang_arr['day'] ='يوم';
+$lang_arr['month'] ='شهر';
+?>
